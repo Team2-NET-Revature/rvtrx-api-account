8b8ef89a930672ca58e3d52b875574b841596f7a
Daniel Aasa <danielaasa42@gmail.com>
2020-08-14 12:55:55 -0700

    misc for my station docker to work

--- aspnet/RVTR.Account.DataContext/AccountContext.cs
+++ aspnet/RVTR.Account.DataContext/AccountContext.cs
@@ -6,25 +6,25 @@ using System;
 
 namespace RVTR.Account.DataContext
 {
-  /// <summary>
-  /// Represents the _Account_ context
-  /// </summary>
-  public class AccountContext : DbContext
-  {
-    public DbSet<AccountModel> Accounts { get; set; }
-    public DbSet<ProfileModel> Profiles { get; set; }
-
-    public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    /// <summary>
+    /// Represents the _Account_ context
+    /// </summary>
+    public class AccountContext : DbContext
     {
-      modelBuilder.Entity<AccountModel>().HasKey(e => e.Id);
-      modelBuilder.Entity<AddressModel>().HasKey(e => e.Id);
-      modelBuilder.Entity<BankCardModel>().HasKey(e => e.Id);
-      modelBuilder.Entity<NameModel>().HasKey(e => e.Id);
-      modelBuilder.Entity<PaymentModel>().HasKey(e => e.Id);
-      modelBuilder.Entity<ProfileModel>().HasKey(e => e.Id);
-      
+        public DbSet<AccountModel> Accounts { get; set; }
+        public DbSet<ProfileModel> Profiles { get; set; }
+
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountModel>().HasKey(e => e.Id);
+            modelBuilder.Entity<AddressModel>().HasKey(e => e.Id);
+            modelBuilder.Entity<BankCardModel>().HasKey(e => e.Id);
+            modelBuilder.Entity<NameModel>().HasKey(e => e.Id);
+            modelBuilder.Entity<PaymentModel>().HasKey(e => e.Id);
+            modelBuilder.Entity<ProfileModel>().HasKey(e => e.Id);
+
+        }
     }
-  }
 }
