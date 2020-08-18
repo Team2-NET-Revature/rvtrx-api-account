370485a4091aa44e1d9a7796fdcb5a17ba3e267c
Quentin Bullock <bullock.quentin@gmail.com>
2020-08-18 19:09:11 -0400

    Formatting updates

--- aspnet/RVTR.Account.DataContext/AccountContext.cs
+++ aspnet/RVTR.Account.DataContext/AccountContext.cs
@@ -4,26 +4,26 @@ using RVTR.Account.ObjectModel.Models;
 
 namespace RVTR.Account.DataContext
 {
-    /// <summary>
-    /// Represents the _Account_ context
-    /// </summary>
-    public class AccountContext : DbContext
+  /// <summary>
+  /// Represents the _Account_ context
+  /// </summary>
+  public class AccountContext : DbContext
+  {
+    public DbSet<AccountModel> Accounts { get; set; }
+    public DbSet<ProfileModel> Profiles { get; set; }
+    public DbSet<PaymentModel> Payments { get; set; }
+    public DbSet<AddressModel> Addresses { get; set; }
+
+    public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DbSet<AccountModel> Accounts { get; set; }
-        public DbSet<ProfileModel> Profiles { get; set; }
-        public DbSet<PaymentModel> Payments { get; set; }
-        public DbSet<AddressModel> Addresses { get; set; }
+      modelBuilder.Entity<AccountModel>().HasKey(e => e.Id);
+      modelBuilder.Entity<AddressModel>().HasKey(e => e.Id);
+      modelBuilder.Entity<BankCardModel>().HasKey(e => e.Id);
+      modelBuilder.Entity<PaymentModel>().HasKey(e => e.Id);
+      modelBuilder.Entity<ProfileModel>().HasKey(e => e.Id);
 
-        public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<AccountModel>().HasKey(e => e.Id);
-            modelBuilder.Entity<AddressModel>().HasKey(e => e.Id);
-            modelBuilder.Entity<BankCardModel>().HasKey(e => e.Id);
-            modelBuilder.Entity<PaymentModel>().HasKey(e => e.Id);
-            modelBuilder.Entity<ProfileModel>().HasKey(e => e.Id);
-
-        }
     }
+  }
 }
